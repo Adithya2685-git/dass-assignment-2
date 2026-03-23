4ab785c3c02fb8bbc61b687df7bb4bea71bd16d7
Adithya <adithya.jillellamudi@students.iiit.ac.in>
2026-03-23 19:14:05 +0530

    Error 12: net_worth ignored owned property value

--- whitebox/tests/test_player.py
+++ whitebox/tests/test_player.py
@@ -2,6 +2,7 @@
 
 from moneypoly.config import GO_SALARY, STARTING_BALANCE
 from moneypoly.player import Player
+from moneypoly.property import Property
 
 
 def test_move_awards_salary_when_passing_go():
@@ -13,3 +14,13 @@ def test_move_awards_salary_when_passing_go():
 
     assert new_position == 1
     assert player.balance == STARTING_BALANCE + GO_SALARY
+
+
+def test_net_worth_includes_owned_property_value():
+    """Owned property value should count toward net worth."""
+    player = Player("Owner", balance=200)
+    prop = Property(("Lot", 1, 100, 10))
+    prop.owner = player
+    player.add_property(prop)
+
+    assert player.net_worth() == 300
