5992338f0ff3c516507a190e057dbfcf24190182
Adithya <adithya.jillellamudi@students.iiit.ac.in>
2026-03-23 22:22:44 +0530

    Add reputation module

--- integration/code/streetrace_manager/reputation.py
+++ integration/code/streetrace_manager/reputation.py
@@ -3,6 +3,8 @@ from .state import StreetRaceState
 
 def add_reputation(state: StreetRaceState, amount: int) -> int:
     state.reputation += amount
+    if state.reputation < 0:
+        state.reputation = 0
     return get_reputation(state)
 
 
